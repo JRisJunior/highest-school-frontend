9305cdbf9a8ac26268efee5d7f862dc72b30090d
JRisJunior <jeffmrubin@gmail.com>
2023-02-07 12:07:39 -0600

    index action

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,29 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { EventsIndex } from "./EventsIndex";
+
 export function Content() {
+
+  // const events = [
+  //   {id: 1, subject: "Networking", description: "you will reach out to someone you know", user_id: 1, start_time: "2022-01-01 10:00:00", end_time: "2022-01-01 11:00:00" },
+  //   {id: 2, subject: "Gym", description: "You will workout", user_id: 1, start_time: "2022-01-01 12:00:00", end_time: "2022-01-01 13:00:00" }
+  // ];
+
+  const [events, setEvents] = useState([]);
+
+  const handleIndexEvents = () => {
+    console.log("handleIndexEvents");
+    axios.get("http://localhost:3000/events.json").then((response) => {
+      console.log(response.data);
+      setEvents(response.data);
+    });
+  };
+
+  useEffect(handleIndexEvents, []);
+
   return (
     <div>
-      <h1>Welcome to React!</h1>
+      <EventsIndex events={events} />
     </div>
   );
 }
