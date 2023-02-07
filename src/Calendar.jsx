e70da8bf7d496ddfa72226a227a28a53e10f185d
JRisJunior <jeffmrubin@gmail.com>
2023-02-07 15:43:57 -0600

    show hardcoded data on calendar

--- src/Calendar.jsx
+++ src/Calendar.jsx
@@ -54,6 +54,44 @@ class Calendar extends Component {
     return this.calendarRef.current.control;
   }
 
+  componentDidMount() {
+
+    const events = [
+      {
+        id: 1,
+        text: "Event 1",
+        start: "2023-02-07T10:30:00",
+        end: "2023-02-07T13:00:00"
+      },
+      {
+        id: 2,
+        text: "Event 2",
+        start: "2023-02-08T09:30:00",
+        end: "2023-02-08T11:30:00",
+        backColor: "#6aa84f"
+      },
+      {
+        id: 3,
+        text: "Event 3",
+        start: "2023-02-08T12:00:00",
+        end: "2023-02-08T15:00:00",
+        backColor: "#f1c232"
+      },
+      {
+        id: 4,
+        text: "Event 4",
+        start: "2023-02-06T11:30:00",
+        end: "2023-02-06T14:30:00",
+        backColor: "#cc4125"
+      },
+    ];
+
+    const startDate = "2023-03-07";
+
+    this.calendar.update({startDate, events});
+
+  }
+
   render() {
     const {...config} = this.state;
     return (
